5a5ffc0ac3dfef228e38263a16692c3a573c9e9a
seolran <leesugar90@gmail.com>
2019-10-04 18:00:26 +0900

    [Component styling] + install node-sass + yarn eject + sass-loader 설정 커스터마이징

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import React, { Component } from "react";
+import SassComponent from "./SassComponent";
 
 class App extends Component {
-    render() {
-        return <SassComponent />;
-    }
-}
+  render() {
+    return <SassComponent />;
+  }
+} 
 
 export default App;
