2a8c89dd88185938570c342338fd2488dbee51be
설설이 <leesugar90@gmail.com>
2019-10-09 21:11:29 +0900

    [news-viewer] - 외부 api 연동하기 + Category기능 구현-seolran + Category기능 Router로 구현-seolran + styled components사용하여 category메뉴 스타일링

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import NewsList from './components/NewsList';
+import styled from 'styled-components';
+
+const CategoryBlock = styled.div`
+    width: 768px;
+    margin: 2rem auto;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        li {
+            flex: 1 1 auto;
+            text-align: center;
+            a {
+                display: inline-block;
+                padding: 0.5rem 1rem;
+                color: #538ae4;
+                text-decoration: none;
+                border-radius: 5rem;
+                background-color: rgba(83, 138, 228, 0.2);
+                &:hover {
+                    background-color: rgba(83, 138, 228, 0.6);
+                    color: #fff;
+                    transition: all 0.4s ease-out;
+                }
+            }
+        }
+    }
+`;
 
 class App extends Component {
     state = {
@@ -17,14 +53,15 @@ class App extends Component {
     render() {
         return (
             <div>
-                <ul>
-                    {this.state.categories.map((category, index) => (
-                        <li key={index}>
-                            <Link to={category}>{category}</Link>
-                        </li>
-                    ))}
-                </ul>
-
+                <CategoryBlock>
+                    <ul>
+                        {this.state.categories.map((category, index) => (
+                            <li key={index}>
+                                <Link to={category}>{category}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </CategoryBlock>
                 <Route path="/:category" component={NewsList} />
             </div>
         );
